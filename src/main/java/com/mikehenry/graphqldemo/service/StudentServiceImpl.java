ffca034c3b8edfe3ab471c22e5611d5134ca5fe6
mikemacharia39 <mikemacharia39@gmail.com>
2022-05-18 19:28:36 +0300

    Add implementation for getting student and all students

--- src/main/java/com/mikehenry/graphqldemo/service/StudentServiceImpl.java
+++ src/main/java/com/mikehenry/graphqldemo/service/StudentServiceImpl.java
@@ -1,17 +1,34 @@
 package com.mikehenry.graphqldemo.service;
 
+import com.mikehenry.graphqldemo.model.Student;
+import com.mikehenry.graphqldemo.repositiory.StudentRepository;
 import com.mikehenry.graphqldemo.response.StudentResponse;
+import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class StudentImpl implements Student {
+@RequiredArgsConstructor
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
     @Override
     public StudentResponse getStudent(Long id) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isEmpty()) {
+            throw new RuntimeException("No student found");
+        }
 
+        return new StudentResponse(optionalStudent.get());
     }
 
     @Override
     public List<StudentResponse> getAllStudents() {
-        return null;
+        List<StudentResponse> studentResponses = new ArrayList<>();
+        List<Student> students = studentRepository.findAll();
+        students.forEach(student -> studentResponses.add(new StudentResponse(student)));
+        return studentResponses;
     }
 }
