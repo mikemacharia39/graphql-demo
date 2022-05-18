9e9bc36a0b35a19ad357c16c86bac9713c56f174
mikemacharia39 <mikemacharia39@gmail.com>
2022-05-18 19:14:31 +0300

    define the response class

--- src/main/java/com/mikehenry/graphqldemo/response/StudentResponse.java
+++ src/main/java/com/mikehenry/graphqldemo/response/StudentResponse.java
@@ -1,2 +1,30 @@
-package com.mikehenry.graphqldemo.response;public class StudentResponse {
+package com.mikehenry.graphqldemo.response;
+
+import com.mikehenry.graphqldemo.model.Student;
+import com.mikehenry.graphqldemo.model.StudentCourse;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+public class StudentResponse {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private String address;
+    private Set<StudentCourse> studentCourses;
+
+    public StudentResponse(Student student) {
+        this.id = student.getId();
+        this.firstName = student.getFirstName();
+        this.lastName = student.getLastName();
+        this.dob = student.getDob();
+        this.address = student.getAddress();
+        this.studentCourses.addAll(student.getStudentCourses());
+    }
 }
