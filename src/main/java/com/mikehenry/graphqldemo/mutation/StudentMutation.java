67b7a354c72b137b88888dbcd86ad4e8a106b2fb
mikemacharia39 <mikemacharia39@gmail.com>
2022-05-29 19:31:02 +0300

    add implementation for create student mutation

--- src/main/java/com/mikehenry/graphqldemo/mutation/StudentMutation.java
+++ src/main/java/com/mikehenry/graphqldemo/mutation/StudentMutation.java
@@ -1,6 +1,17 @@
 package com.mikehenry.graphqldemo.mutation;
 
+import com.mikehenry.graphqldemo.request.CreateStudentRequest;
+import com.mikehenry.graphqldemo.response.StudentResponse;
+import com.mikehenry.graphqldemo.service.StudentService;
 import graphql.kickstart.tools.GraphQLMutationResolver;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class StudentMutation implements GraphQLMutationResolver {
+
+    private final StudentService studentService;
+
+    public StudentResponse createStudent(CreateStudentRequest createStudentRequest) {
+        return studentService.createStudent(createStudentRequest);
+    }
 }
--- src/main/java/com/mikehenry/graphqldemo/service/StudentService.java
+++ src/main/java/com/mikehenry/graphqldemo/service/StudentService.java
@@ -1,10 +1,13 @@
 package com.mikehenry.graphqldemo.service;
 
+import com.mikehenry.graphqldemo.request.CreateStudentRequest;
 import com.mikehenry.graphqldemo.response.StudentResponse;
 
 import java.util.List;
 
 public interface StudentService {
+
+    StudentResponse createStudent(final CreateStudentRequest createStudentRequest);
 
     StudentResponse getStudent(Long id);
 
