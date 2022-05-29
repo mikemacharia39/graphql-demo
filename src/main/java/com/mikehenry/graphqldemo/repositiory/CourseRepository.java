2a738b754809eb24005b0f6fc250d78429693a04
mikemacharia39 <mikemacharia39@gmail.com>
2022-05-29 19:12:03 +0300

    add course repository

--- src/main/java/com/mikehenry/graphqldemo/repositiory/CourseRepository.java
+++ src/main/java/com/mikehenry/graphqldemo/repositiory/CourseRepository.java
@@ -1,2 +1,9 @@
-package com.mikehenry.graphqldemo.repositiory;public interface CourseRepository {
+package com.mikehenry.graphqldemo.repositiory;
+
+import com.mikehenry.graphqldemo.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
 }
