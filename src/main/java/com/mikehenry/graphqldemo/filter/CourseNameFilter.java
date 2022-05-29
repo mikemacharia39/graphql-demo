4bb84bc82adff643630e971e8e3f41cd991d118a
mikemacharia39 <mikemacharia39@gmail.com>
2022-05-29 16:41:47 +0300

    This will focus on graphql edges mapping student to course and vise versa

---
+++ src/main/java/com/mikehenry/graphqldemo/filter/CourseNameFilter.java
@@ -0,0 +1,16 @@
+package com.mikehenry.graphqldemo.filter;
+
+public enum CourseNameFilter {
+    CCC_001,
+    CCC_002,
+    HIV_003,
+    CCC_004,
+    CCC_005,
+    CCC_006,
+    CCC_007,
+    CCC_008,
+    CCC_009,
+    CCC_0010,
+    CCC_0011,
+    CCC_0012
+}
